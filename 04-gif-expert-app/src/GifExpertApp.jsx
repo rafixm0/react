912e588288b47040e561d6dc79a64c4a3719dc2f
Rafix <rafacpe@gmail.com>
2023-08-19 20:54:32 +0200

    proyecto React empezando

---
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './styles.css';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState([ 'One Punch', 'Dragon Ball']);
+
+    const onAddCategory = ( newCategory) => {
+
+      if ( categories.includes(newCategory)) return;
+
+      setCategories([newCategory, ...categories]);
+      
+      //categories.push(newCategory);
+      //console.log(categories);
+      //setCategories([newCategory, ...categories]);
+      //setCategories( cat => [...cat, 'Valorant']);
+    }
+
+  return (
+    <>
+        <h1>GifExpertApp</h1>
+
+        <AddCategory 
+          onNewCategory={ (value) => onAddCategory(value)}
+        />
+
+      
+        { 
+          categories.map( (category) => (
+            <GifGrid 
+              key = { category } 
+              category = { category } />
+            ))
+        }
+      
+
+     
+        
+
+    </>
+  )
+}
