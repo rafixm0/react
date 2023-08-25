c3106e7d82a4c53632e5a3b3c8ce3482b2e46b6b
Rafix <rafacpe@gmail.com>
2023-08-25 18:56:34 +0200

    empezando con los tests

--- 05-hook-app/src/hooks/useCounter.js
+++ 05-hook-app/src/hooks/useCounter.js
@@ -6,7 +6,7 @@ export const useCounter = ( initialValue = 10 ) => {
     const [counter, setCounter] = useState(initialValue);
 
     const increment = () => {
-        setCounter( counter + 1 );
+        setCounter( (current) => counter + 1 );
     }
 
     const decrement = () => {
